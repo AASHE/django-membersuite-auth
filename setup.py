0a0cdd3fe8b39e7db43657264c7cbc0015c062d6
Robert Erb <bob.erb@gmail.com>
2017-05-01 00:29:01 -0400

    Update version of membersuite_api_client

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name="django-membersuite-auth",
-      version="1.4",
+      version="1.4.1",
       description="Django Authentication By MemberSuite",
       author=("Association for the Advancement of Sustainability in "
               "Higher Education"),
