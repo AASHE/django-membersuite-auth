3e47f39e0cbf9297345ef476fdaf33b690bae78a
Robert Erb <bob.erb@gmail.com>
2018-06-17 22:34:39 -0400

    Upgrade to v1.6.5 membersuite-api-client

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name="django-membersuite-auth",
-      version="1.6.3",
+      version="1.6.5",
       description="Django Authentication By MemberSuite",
       author=("Association for the Advancement of Sustainability in "
               "Higher Education"),
@@ -31,6 +31,6 @@ setup(name="django-membersuite-auth",
       ],
       include_package_data=True,
       install_requires=["future",
-                        "membersuite-api-client==0.4.4"
+                        "membersuite-api-client==0.4.6"
       ]
 )  # noqa what's visual indentation?
